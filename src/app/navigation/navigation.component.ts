d4bd583c990aecd0e9b774fbd42b1342deeb99a2
pablo <marce_1993_1_@hotmail.com>
2019-02-28 18:02:16 -0300

    menu translation

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -3,6 +3,7 @@ import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { ScrollService } from 'src/app/services/scroll.service';
+import { TranslateService } from '../../../node_modules/@ngx-translate/core';
 
 @Component({
   selector: 'app-navigation',
@@ -15,9 +16,15 @@ export class NavigationComponent {
       map(result => result.matches)
     );
     
-  constructor(private breakpointObserver: BreakpointObserver, private scrollContentService: ScrollService) {
+  constructor(private translate: TranslateService, private breakpointObserver: BreakpointObserver, private scrollContentService: ScrollService) {
+        // this language will be used as a fallback when a translation isn't found in the current language
+        translate.setDefaultLang('en');
 
+         // the lang to use, if the lang isn't available, it will use the current loader to get them
+        translate.use('en');
   }
+
+  selected = 'en';
 
   langs: any[] = [
     {value: 'es', viewValue: 'Spanish'},
@@ -26,7 +33,7 @@ export class NavigationComponent {
   ];
 
   changeLanguage(lang: any){
-    console.log(lang.value);
+    this.translate.use(lang.value);
   }
 
   onScroll() {
