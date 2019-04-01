cf0be74f13b94de8d9496d18949ce0f8af3b7dd5
pablo <pablo.m.bianco@hotmail.com>
2019-03-31 23:23:38 -0300

    --

--- src/app/navigation/upload/upload.component.ts
+++ src/app/navigation/upload/upload.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-upload',
@@ -8,14 +8,50 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class UploadComponent implements OnInit {
   isLinear = true;
+  upload_form = new FormGroup({});
 
-  constructor(private changeDetectorRef: ChangeDetectorRef) {
+  fileTypes: ["Video","Application", "Game" , "Document"];
+
+  fileSubtypes: [
+    {
+      type: "Video",
+      subtypes: ["Anime", "Movie", "Serie", "Documental", "Tutorial"]
+    },
+    {
+      type: "Application",
+      subtypes: ["Game", "Application", "Serie", "Documental", "Tutorial"]
+    }
+  ];
+
+  constructor(private changeDetectorRef: ChangeDetectorRef, private formBuilder: FormBuilder) {
     setInterval(() => {
       // require view to be updated
       this.changeDetectorRef.markForCheck();
     }, 500);
   }
   
+  createFormGroup(){
+    return new FormGroup({
+      categoryForm: new FormGroup({
+        fileType: new FormControl('', Validators.required)
+      }),
+      postForm: new FormGroup({
+        title: new FormControl('', Validators.required),
+        body: new FormControl('', Validators.required),
+        image: new FormControl('', Validators.required),
+      }),
+      fileForm: new FormGroup({
+        filehash: new FormControl('', Validators.required),
+        tfilehash: new FormControl('', Validators.required),
+        subtitles: this.formBuilder.group({
+          sub_titles: this.formBuilder.array([])
+        }),
+        body: new FormControl('', Validators.required),
+      }),
+      userForm: new FormGroup({})
+    });
+  }
+
   ngOnDestroy() {
     this.changeDetectorRef.detach();
   }
---
+++ src/assets/configs.ts
@@ -0,0 +1,6 @@
+export var permittedFormats: {
+    media: ["3gp", "avi", "mov", "mp4", "m4v", "m4a", "mp3", "mkv", "ogv", "ogm", "ogg", "oga", "webm", "wav"],
+    subtitles: ["vtt"],
+    images: ["bmp", "gif", "jpg", "jpeg", "png", "webp"],
+    books: ["pdf", "epub"],
+}
